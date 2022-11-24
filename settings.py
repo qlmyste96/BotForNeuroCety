f7cc9fb7819c56ef4724361161b7bebf04908620
qlmyste96 <90056573+qlmyste96@users.noreply.github.com>
2022-11-24 23:12:55 +0200

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+# Loading .env variables
+load_dotenv(find_dotenv())
+
+TELEGRAM_TOKEN = "5654171596:AAHjYlyvYbgmQDxqsP76SR0KBzcW0C6PSd8"
+if TELEGRAM_TOKEN is None:
+    raise Exception("Please setup the .env variable TELEGRAM_TOKEN.")
+
+PORT = int(os.environ.get('PORT', '8443'))
+HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
+
+TELEGRAM_SUPPORT_CHAT_ID = -1001875629835
+if TELEGRAM_SUPPORT_CHAT_ID is None or not str(TELEGRAM_SUPPORT_CHAT_ID).lstrip("-").isdigit():
+    raise Exception("You need to specify 'TELEGRAM_SUPPORT_CHAT_ID' env variable: The bot will forward all messages to this chat_id. Add this bot https://t.me/ShowJsonBot to your private chat to find its chat_id.")
+TELEGRAM_SUPPORT_CHAT_ID = int(TELEGRAM_SUPPORT_CHAT_ID)
+
+
+WELCOME_MESSAGE = os.getenv("WELCOME_MESSAGE", "👋")
+REPLY_TO_THIS_MESSAGE = os.getenv("REPLY_TO_THIS_MESSAGE", "REPLY_TO_THIS")
+WRONG_REPLY = os.getenv("WRONG_REPLY", "WRONG_REPLY")
